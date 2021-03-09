c0ccde618c6fb5579708bf95734eafb750e38001
Lucas Pereira <lucaspfernandes1994@hotmail.com>
2021-03-09 16:23:01 -0300

    Projeto criado

---
+++ src/main/java/com/springrest/cursospringrest/CursospringrestApplication.java
@@ -0,0 +1,13 @@
+package com.springrest.cursospringrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursospringrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursospringrestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springrest/cursospringrest/CursospringrestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springrest.cursospringrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursospringrestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
