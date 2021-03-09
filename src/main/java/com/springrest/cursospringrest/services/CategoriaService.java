696fc1d7ed6b34568c0328db20aa7102ac9404d3
Lucas Pereira <lucaspfernandes1994@hotmail.com>
2021-03-09 19:05:06 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/springrest/cursospringrest/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.springrest.cursospringrest.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.springrest.cursospringrest.domain.Categoria;
+import com.springrest.cursospringrest.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+	}
+
+}
