a6b33731ff7c21e3a357da4813119e910918b315
Lucas Pereira <lucaspfernandes1994@hotmail.com>
2021-03-11 18:32:32 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/springrest/cursospringrest/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.springrest.cursospringrest.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.springrest.cursospringrest.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
+
+}
