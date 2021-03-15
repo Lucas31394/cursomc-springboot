6a6952bc7eefc2463cfa99eb3acfa64df30ccd42
Lucas Pereira <lucaspfernandes1994@hotmail.com>
2021-03-15 10:59:49 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/springrest/cursospringrest/services/CategoriaService.java
+++ src/main/java/com/springrest/cursospringrest/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.springrest.cursospringrest.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.springrest.cursospringrest.domain.Categoria;
 import com.springrest.cursospringrest.repositories.CategoriaRepository;
+import com.springrest.cursospringrest.services.exceptions.DataIntegrityException;
 import com.springrest.cursospringrest.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,6 +31,16 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
+		}
 	}
 
 }
