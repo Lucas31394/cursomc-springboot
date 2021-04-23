1d7d14360a1273a6bdbad4a26bf35cdc3a013a87
Lucas Pereira <lucaspfernandes1994@hotmail.com>
2021-04-23 12:26:56 -0300

    Enviando imagem via endpoint

--- src/main/java/com/springrest/cursospringrest/CursospringrestApplication.java
+++ src/main/java/com/springrest/cursospringrest/CursospringrestApplication.java
@@ -1,17 +1,11 @@
 package com.springrest.cursospringrest;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.springrest.cursospringrest.services.S3Service;
-
 @SpringBootApplication
 public class CursospringrestApplication implements CommandLineRunner {
-	
-	@Autowired
-	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringrestApplication.class, args);
@@ -19,7 +13,6 @@ public class CursospringrestApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\temp\\fotos\\188300-004-0F2FF046.jpg");
 	}
 
 }
